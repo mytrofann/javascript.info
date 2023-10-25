912a69357fae95d9ed5263808bdc20d34c78de52
Dmitriy <mytrofann@gmail.com>
2023-10-25 20:07:16 +0300

    Delete

---
+++ scripts/alert.js
@@ -0,0 +1,10 @@
+// function sumSliceArray(arr, first, second) {
+//   // write function code here
+//   if (!arr || !Array.isArray(arr) || arr.length === 0) {
+//     return [];
+//   }
+//   arr[first]
+//   arr[second]
+
+
+// }
---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+document.addEventListener('partialsLoaded', () => {
+    import('./alert.js');
+});
